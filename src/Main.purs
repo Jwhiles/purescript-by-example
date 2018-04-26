0b339c1a81e480d704f9d1a9d225480fa9ab2c4d
John Whiles <john.whiles@thetrainline.com>
2018-04-26 10:31:37 +0100

    structure exercises

--- src/Main.purs
+++ src/Main.purs
@@ -1,11 +1,11 @@
 module Main where
 
-import Control.Monad.Eff.Console
-import Prelude
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff (Eff)
+import Prelude (Unit)
 import Ch2
 
 
+main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "Chapter 2 exercises"
-  logShow (diagonal 3.0 4.0)
-  logShow (diagonal 5.0 4.0)
+  ch2
--- src/chapter2/ch2.purs
+++ src/chapter2/ch2.purs
@@ -1,6 +1,14 @@
 module Ch2 where
 
-import Math (sqrt)
+import Control.Monad.Eff.Console
+import Math (sqrt, pi)
 import Prelude
 
 diagonal w h = sqrt (w * w + h * h)
+
+circleArea r = pi * r * r
+
+ch2 = do
+  log "Chapter 2 exercises"
+  logShow (diagonal 3.0 4.0)
+  logShow (circleArea 3.0)
