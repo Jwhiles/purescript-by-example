7a42a82099c7fa5de5d974f4f936d19f12bacbfe
John Whiles <john.whiles@thetrainline.com>
2018-04-27 17:05:54 +0100

    All exercises for ch3

--- src/chapter3/Data/AddressBook.purs
+++ src/chapter3/Data/AddressBook.purs
@@ -3,29 +3,39 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
-import Data.List (List(..), filter, head)
+import Data.List (List(..), filter, head, some, nub)
 import Data.Maybe (Maybe)
+import Data.Foldable (foldr)
+import Data.Newtype
 
-type Address =
+
+
+tt = (\x -> x.firstName) <<< unwrap 
+
+newtype Address = Address
   { street :: String
   , city   :: String
   , state  :: String
   }
 
-type Entry =
+
+newtype Entry = Entry
   { firstName :: String
   , lastName  :: String
   , address   :: Address
   }
 
+
 type AddressBook = List Entry
 
 showAddress :: Address -> String
-showAddress addr = addr.street <> ", " <> addr.city <> ", " <> addr.state
+showAddress =
+  (\addr -> addr.street <> ", " <> addr.city <> ", " <> addr.state) <<< unwrap
 
 showEntry :: Entry -> String
-showEntry entry = entry.lastName <> ", " <> entry.firstName <> ": " <>
-  showAddress entry.address
+showEntry =
+  (\entry -> entry.lastName <> ", " <> entry.firstName <> ": " <>
+  showAddress entry.address) <<< unwrap
 
 emptyBook :: AddressBook
 emptyBook = empty
@@ -37,5 +47,53 @@ findEntry :: String -> String -> AddressBook -> Maybe Entry
 findEntry firstName lastName = head <<< filter filterEntry
   where
     filterEntry :: Entry -> Boolean
-    filterEntry entry = entry.firstName == firstName && entry.lastName ==
-      lastName
+    filterEntry = (\entry -> entry.firstName == firstName && entry.lastName ==
+      lastName) <<< unwrap
+
+
+printEntry :: String -> String -> AddressBook -> Maybe String
+printEntry firstName lastName book = 
+  map showEntry (findEntry firstName lastName book)
+
+-- exercises
+{-- findAddress :: String -> String -> String -> AddressBook -> Maybe Entry --}
+{-- findAddress street city state = head <<< filter filterAddresses --}
+{--   where --} 
+{--         filterAddresses :: Entry -> Boolean --}
+{--         filterAddresses { address: a } = --} 
+{--           a.street == street && --}
+{--           a.city == city && --}
+{--           a.state == state --}
+
+findAddress :: String -> String -> String -> AddressBook -> Maybe Entry
+findAddress street city state = head <<< filter filterAddresses
+  where 
+        filterAddresses :: Entry -> Boolean
+        filterAddresses = unwrap >>> (\{ address: wa } -> 
+                          (unwrap >>> (\a -> 
+                              a.street == street &&
+                              a.city == city &&
+                              a.state == state)) wa)
+
+
+-- this function could be more efficient - how do we early return if we find
+isNamePresent :: String -> AddressBook -> Boolean
+isNamePresent name addr = foldr test false addr
+  where 
+        test e acc = (unwrap >>> \{ firstName : n } -> n == name || acc) e
+    {-- test { firstName: n } a = n == name || a --}
+
+
+
+derive instance newtypeAddress :: Newtype Address _
+derive instance eqAddress :: Eq Address 
+
+derive instance newtypeEntry :: Newtype Entry _
+derive instance eqEntry :: Eq Entry 
+
+-- deriving these instances allows me to simply use nub because I have EQ.
+-- but adds extra complexity to all the other code. Can this be simplified?
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates addrb = nub addrb
+
--- src/chapter3/ch3.purs
+++ src/chapter3/ch3.purs
@@ -1,2 +1,38 @@
 module Ch3 where
 
+import Control.Monad.Eff.Console
+import Prelude
+import Data.AddressBook
+import Data.Maybe (fromMaybe)
+import Data.Newtype
+
+address :: Address
+address = Address { street: "123 Fake St.", city: "Faketown", state: "CA" }
+
+entry :: Entry
+entry = Entry { firstName: "John", lastName: "Smiles", address: address }
+
+
+ch3 = do
+  log $ tt testBoi
+  log "Chapter 3 exercises"
+  log $ fromMaybe "Not found" $ printEntry "John" "Smiles" book1
+  log $ fromMaybe "Not found" $ printEntry "no one" "Smiles" book1
+  logShow $ isNamePresent "John" book1
+  logShow $ isNamePresent "francine" book1
+  log $ fromMaybe "Not found" $ printEntry "John" "Smiles" book2
+  log $ fromMaybe "Not found" $ printEntry "John" "Smiles" $ removeDuplicates book2
+  {-- logShow $ book2 --}
+    where 
+      book1 = insertEntry entry emptyBook
+      book2 = insertEntry entry book1
+
+
+newtype Test = Test { firstName :: String }
+
+derive instance newtypeTest :: Newtype Test _
+{-- derive instance newtypeShow :: Show Test --}
+
+testBoi = Test { firstName: "john" }
+
+tt = (\x -> x.firstName) <<< unwrap 
